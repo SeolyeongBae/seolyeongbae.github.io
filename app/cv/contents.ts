75c2a61dbd75b9feae15735af4d6c1d973dd8234
seolyeongbae <peixueying@gmail.com>
2024-01-19 14:58:41 +0900

    update scholarship, education

--- app/cv/contents.ts
+++ app/cv/contents.ts
@@ -1,4 +1,4 @@
-const CURRENT = "~ current";
+const CURRENT = "~current";
 
 export const contents = [
   {
@@ -14,8 +14,16 @@ export const contents = [
         additional: "GPA 4.05/4.5",
       },
       {
+        startDate: "2023.09",
+        endDate: "~2023.12",
+        title: "Korea Advanced Institute of Science and Technology",
+        location: "Daejeon, Korea",
+        description: "Fall Semester Exchange Student",
+        additional: "GPA 4.03/4.3",
+      },
+      {
         startDate: "2022.06",
-        endDate: "2022.08",
+        endDate: "~2022.08",
         title: "University of California, Berkeley",
         location: "California, USA",
         description: "UC Berkeley Summer Session",
@@ -68,6 +76,11 @@ export const contents = [
   {
     title: "Honors & Scholarship",
     content: [
+      {
+        startDate: "2023.11",
+        title: "Inseong Hong Bok-soon Scholarship",
+        description: "Gwangju Institute of Science and Technology",
+      },
       {
         startDate: "2022.03",
         title: "National Natural Science and Engineering Scholarship",

8c6502d3b68ad247cbcdc4f09a2c07398c4f3e32
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-07-03 16:11:08 +0900

    chore: update CV

--- app/cv/contents.ts
+++ app/cv/contents.ts
@@ -8,16 +8,16 @@ export const contents = [
         startDate: "2020.03",
         endDate: CURRENT,
         title: "Gwangju Institute of Science and Technology",
-        location: "Gwangju, Korea",
+        location: "Gwangju, South Korea",
         description:
           "Major in Electrical Engineering and Computer Science (EECS)",
-        additional: "GPA 4.05/4.5",
+        additional: "GPA 4.18/4.5",
       },
       {
         startDate: "2023.09",
         endDate: "~2023.12",
         title: "Korea Advanced Institute of Science and Technology",
-        location: "Daejeon, Korea",
+        location: "Daejeon, South Korea",
         description: "Fall Semester Exchange Student",
         additional: "GPA 4.03/4.3",
       },
@@ -34,24 +34,31 @@ export const contents = [
     title: "Experiences",
     content: [
       {
+        startDate: "2024.01",
+        endDate: CURRENT,
+        title: "GIST Visual Recognition and Learning Lab",
+        location: "Gwangju, South Korea",
+        description: "Undergraduate Research Internship",
+      },
+      {
         startDate: "2023.07",
         endDate: "~2023.09",
-        title: "KAIST IC Lab",
-        location: "Daejeon, Korea",
+        title: "KAIST Interactive Computing Lab",
+        location: "Daejeon, South Korea",
         description: "Undergraduate Research Internship",
       },
       {
         startDate: "2022.12",
         endDate: "~2023.06",
         title: "NAVER Cloud",
-        location: "Seongnam, Korea",
+        location: "Seongnam, South Korea",
         description: "Full Time Software Engineering Internship",
       },
       {
         startDate: "2022.04",
         endDate: "~2022.11",
         title: "Software Maestro 13th",
-        location: "Seoul, Korea",
+        location: "Seoul, South Korea",
         description:
           "Development of Belloga, a crowdsourcing service with mobile alarm",
         additional: "Collaborated with AI solution company Upstage",
@@ -65,11 +72,20 @@ export const contents = [
       {
         startDate: "2023",
         title:
-          " MindfulDiary: Harnessing Large Language Model to Support Psychiatric Patients’ Journaling",
+          "MindfulDiary: Harnessing Large Language Model to Support Psychiatric Patients’ Journaling",
         description:
           "Taewan Kim, Seolyeong Bae, Hyun Ah Kim, Su-woo Lee, HwajungHong, Chanmo Yang, Young-Ho Kim",
         additional: "[arXiv]",
         url: "https://arxiv.org/abs/2310.05231",
+      },
+      {
+        startDate: "2023",
+        title:
+          " PriviAware: Exploring Data Visualization and Dynamic Privacy Control Support for Data Collection in Mobile Sensing Research",
+        description:
+          "Hyunsoo Lee, Yugyeong Jung, Hei Yiu Law, Seolyeong Bae, and Uichin Lee",
+        additional: "[DOI]",
+        url: "https://dl.acm.org/doi/10.1145/3613904.3642815",
       },
     ],
   },
